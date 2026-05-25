c47a0e06bcd729d4d2b943519d7669b2104cf130
Osc-Ort <oscarortegarios@gmail.com>
2026-05-25 10:29:14 +0200

    new file:   triangular.m

---
+++ triangular.m
@@ -0,0 +1,26 @@
+function [X,Y] = triangular(a,m,b,muestras,tam_extra)
+
+if nargin < 2
+    error("Noy enought arguments.")
+end
+
+if nargin < 3
+    muestras = 1000;
+end
+
+if nargin < 4
+    tam_extra = (d-a) / 2;
+end
+
+if a > b || a > m || b < m
+    error("a,m,b have to be in order.")
+end
+
+X = linspace(a - tam_extra,b + tam_extra,muestras);
+
+Y = [ 
+    zeros(1,sum(X <= a)) ...
+    ((X(X > a & X <= m) - a)) / (m - a) ...
+    (b - (X(X > m & X < b))) / (b - m) ...
+    zeros(1,sum(X >= b))
+];
